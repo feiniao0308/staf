851df465e92b36babd81a2f1c55140dce81d981d
Karthik Vishwanath <kviswanat@bn.com>
2014-02-04 14:16:38 -0500

    working staf

--- src/test/java/com/bn/automation/scripts/Anno.java
+++ src/test/java/com/bn/automation/scripts/Anno.java
@@ -4,11 +4,15 @@ import com.bn.automation.staf.core.TestID;
 
 public class Anno {
 
-	
-	@TestID(info = "123")
-	public static void main(String param){
-		System.out.println();
-		//System.out.println(arg.toString());
-		//System.out.println(info);
+	public static void main(String[] arg) {
+		System.out.println("hello");
 	}
+
+	/*@TestID(info = "123")
+	public static void newmethod(String param) {
+		System.out.println();
+		// System.out.println(arg.toString());
+		System.out.println(param);
+	}*/
+
 }
