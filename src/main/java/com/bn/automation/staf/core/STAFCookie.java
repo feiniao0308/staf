951d049fd978c46cfc3e312ddafec1e5fe537cb2
Karthik Vishwanath <kviswanat@bn.com>
2014-02-17 20:59:37 +0000

    cookie verification implemented

---
+++ src/main/java/com/bn/automation/staf/core/STAFCookie.java
@@ -0,0 +1,91 @@
+package com.bn.automation.staf.core;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Cookie;
+
+public class STAFCookie {
+
+	private static final Logger logger = LogManager.getLogger();
+	private Verify verify = new Verify();
+	Set<Cookie> cookies = STAFDriver.getiDriver().manage().getCookies();
+
+	public void verifyName(Map<String, String> verifyCookie) {
+
+		for (Map.Entry<String, String> entry : verifyCookie.entrySet()) {
+			String s1 = entry.getKey();
+			String s2 = entry.getValue();
+			System.out.println(s1);
+			System.out.println(s2);
+
+			if (s1.equals("NAME")) {
+				verifyName(s2);
+			}
+
+		}
+	}
+
+	public void verifyDomain(Map<String, String> verifyCookie) {
+
+		for (Map.Entry<String, String> entry : verifyCookie.entrySet()) {
+			String s1 = entry.getKey();
+			String s2 = entry.getValue();
+			System.out.println(s1);
+			System.out.println(s2);
+
+			if (s1.equals("DOMAIN")) {
+				verifyDomain(s2);
+			}
+		}
+
+	}
+
+	public void verifyName(String cookieName) {
+
+		int nameCount = 0;
+		Iterator<Cookie> itr = cookies.iterator();
+
+		while (itr.hasNext()) {
+			Cookie c = itr.next();
+			if (c.getName().equals(cookieName)) {
+				nameCount++;
+			}
+
+		}
+		if (nameCount > 0) {
+			logger.info("Verify that cookie named->" + cookieName
+					+ " is set in this page");
+			verify.verifyTrue(true, "Cookie Named->" + cookieName
+					+ " is not set in this page");
+		}
+
+	}
+
+	public void verifyDomain(String cookieDomain) {
+		int domainCount = 0;
+		Iterator<Cookie> itr = cookies.iterator();
+
+		while (itr.hasNext()) {
+			Cookie c = itr.next();
+			if (c.getDomain().equals(cookieDomain)) {
+				domainCount++;
+			}
+
+			logger.trace("Cookie Name: " + c.getName() + "\nCookie Domain: "
+					+ c.getDomain() + "\nCookie Value: " + c.getValue()
+					+ "\nPath: " + c.getPath() + "\nExpiry Date: "
+					+ c.getExpiry() + "\nSecure: " + c.isSecure());
+		}
+		if (domainCount > 0) {
+			logger.info("Verify that cookie domain->" + cookieDomain
+					+ " is set in this page");
+			verify.verifyTrue(true, "Cookie Domain->" + cookieDomain
+					+ " is not set in this page");
+		}
+	}
+
+}
---
+++ src/test/java/com/bn/automation/scripts/CookieVerify.java
@@ -0,0 +1,41 @@
+package com.bn.automation.scripts;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+
+import com.bn.automation.scripts.SO.SOinner;
+import com.bn.automation.staf.core.STAFDriver;
+import com.bn.automation.staf.helpers.STAFConstant;
+
+public class CookieVerify {
+
+	public static void main(String[] args) throws Throwable {
+		STAFDriver driver = STAFDriver.getInstance(STAFConstant.FIREFOX);
+		driver.get("http://qwecweb01.hq.bn-corp.com/bn/");
+		driver.setTestCaseID("167");
+		driver.setDataFileLocation("\\src\\test\\resources\\data\\xmlDataFormat.xml");
+		driver.findElement(By.id("signInLink")).click();
+		Thread.sleep(1000L);
+		SO so = new SO();
+		driver.autopopulate("loginbn", so);
+		Thread.sleep(1000L);
+		SOinner.emailbn.submit();
+		Thread.sleep(2000L);
+		
+		driver.cookieVerify("cookiebn");
+		/*Set<Cookie> cookies = driver.manage().getCookies();
+        Iterator<Cookie> itr = cookies.iterator();
+
+        while (itr.hasNext())
+        {
+            Cookie c = itr.next();
+            System.out.println("--------------------->");
+            System.out.println("Cookie Name: " + c.getName()  + "\nCookie Domain: " + c.getDomain() +  "\nCookie Value: " + c.getValue() +  "\nPath: " + c.getPath()+  "\nExpiry Date: " + c.getExpiry()+  "\nSecure: " + c.isSecure());
+            System.out.println("<-------------------\n");
+        }*/
+	}
+	
+}
